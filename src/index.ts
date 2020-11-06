cb2a82c19c2c19ddad0fb5785ed8468c08e54440
thr0w <JoseFernandoTolentino@gmail.com>
2020-11-06 05:23:09 -0300

    typings

--- src/index.ts
+++ src/index.ts
@@ -24,73 +24,73 @@ export default (() => {
     visitor: {
       ExpressionStatement(path, state) {
         const expr: NodePath<t.CallExpression> = path.get('expression') as any
-        if (expr && expr.isCallExpression()) {
+        if (!(expr && expr.isCallExpression())) return
 
-          const callee: NodePath<t.MemberExpression> = expr.get("callee") as any;
+        const callee: NodePath<t.MemberExpression> = expr.get("callee") as any;
 
-          if (!callee.isMemberExpression()) return;
+        if (!callee.isMemberExpression()) return;
 
-          const object = callee.get("object");
+        const object = callee.get("object");
 
-          const dbgidentifier = state.opts.identifier || 'H5'
-          const isH5 = object.isIdentifier({ name: dbgidentifier })
-            && !object.scope.getBinding(dbgidentifier)
-            && object.scope.hasGlobal(dbgidentifier)
-          if (isH5) {
-            const mode = state.opts.mode || process.env.NODE_ENV || 'development'
-            if (/^production$/gi.test(mode)) {
-              path.remove();
-            } else {
-              const property = callee.get("property");
-              if (property.isIdentifier({ name: 'LOG' })) transpileLOG()
-              else if (property.isIdentifier({ name: 'ASSERT' })) transpileASSERT()
-              else if (property.isIdentifier({ name: 'TRACE' }) || property.isIdentifier({ name: 'CHECK' })) {
-                path.get('expression').replaceWith(
-                  t.logicalExpression('&&', t.identifier(dbgidentifier), t.clone(expr.node))
-                )
-              }
-              else if (property.isIdentifier({ name: 'INIT' })) {
-                const arg0: t.FunctionExpression = expr.node.arguments[0] as any
-                path.get('expression').replaceWith(t.callExpression(t.clone(arg0), []))
-              }
-              else throw path.buildCodeFrameError("Invalid command");
+        const dbgidentifier = state.opts.identifier || 'H5'
+        const isH5 = object.isIdentifier({ name: dbgidentifier })
+          && !object.scope.getBinding(dbgidentifier)
+          && object.scope.hasGlobal(dbgidentifier)
+        if (isH5) {
+          const mode = state.opts.mode || process.env.NODE_ENV || 'development'
+          if (/^production$/gi.test(mode)) {
+            path.remove();
+          } else {
+            const property = callee.get("property");
+            if (property.isIdentifier({ name: 'LOG' })) transpileLOG()
+            else if (property.isIdentifier({ name: 'ASSERT' })) transpileASSERT()
+            else if (property.isIdentifier({ name: 'TRACE' }) || property.isIdentifier({ name: 'CHECK' })) {
+              path.get('expression').replaceWith(
+                t.logicalExpression('&&', t.identifier(dbgidentifier), t.clone(expr.node))
+              )
             }
+            else if (property.isIdentifier({ name: 'INIT' })) {
+              const arg0: t.FunctionExpression = expr.node.arguments[0] as any
+              path.get('expression').replaceWith(t.callExpression(t.clone(arg0), []))
+            }
+            else throw path.buildCodeFrameError("Invalid command");
           }
-          function transpileLOG() {
-            const nexpr = t.callExpression(t.clone(callee.node), [calleeLoc()]
-              .concat(expr.node.arguments.reduce((prev, curr) => {
+        }
+
+        function transpileLOG() {
+          const nexpr = t.callExpression(t.clone(callee.node), [calleeLoc()]
+            .concat(expr.node.arguments.reduce<t.Expression[]>((prev, curr) => {
+              const cclone: t.Expression = t.clone(curr) as any
+              if (t.isStringLiteral(cclone)) prev.push(cclone)
+              else {
+                const cname = t.stringLiteral(generate(cclone).code)
+                prev.push(t.arrayExpression([cname, cclone]))
+              }
+              return prev
+            }, [])))
+          path.get('expression').replaceWith(t.logicalExpression('&&', t.identifier(dbgidentifier), nexpr))
+        }
+        function transpileASSERT() {
+          const nexpr = t.callExpression(t.clone(callee.node), [calleeLoc()]
+            .concat(expr.node.arguments
+              .reduce<t.Expression[]>((prev, curr) => {
                 const cclone: t.Expression = t.clone(curr) as any
-                if (t.isStringLiteral(cclone)) prev.push(cclone)
-                else {
-                  const cname = t.stringLiteral(generate(cclone).code)
-                  prev.push(t.arrayExpression([cname, cclone]))
-                }
+                const cname = t.stringLiteral(generate(cclone).code)
+                prev.push(t.arrayExpression([cname, cclone]))
                 return prev
               }, [])))
-            path.get('expression').replaceWith(t.logicalExpression('&&', t.identifier(dbgidentifier), nexpr))
-          }
-          function transpileASSERT() {
-            const nexpr = t.callExpression(t.clone(callee.node), [calleeLoc()]
-              .concat(expr.node.arguments
-                .reduce((prev, curr) => {
-                  const cclone: t.Expression = t.clone(curr) as any
-                  const cname = t.stringLiteral(generate(cclone).code)
-                  prev.push(t.arrayExpression([cname, cclone]))
-                  return prev
-                }, [])))
-            path.get('expression').replaceWith(
-              t.logicalExpression('&&', t.identifier(dbgidentifier), nexpr)
-            )
-          }
-          function calleeLoc() {
-            const loc = callee.node.loc
-            return t.objectExpression([
-              t.objectProperty(t.identifier('filename'), state.filename ?
-                t.stringLiteral(state.filename.replace(state.cwd, '')) : t.identifier('undefined')),
-              t.objectProperty(t.identifier('line'), t.numericLiteral(loc.start.line)),
-              t.objectProperty(t.identifier('column'), t.numericLiteral(loc.start.column)),
-            ])
-          }
+          path.get('expression').replaceWith(
+            t.logicalExpression('&&', t.identifier(dbgidentifier), nexpr)
+          )
+        }
+        function calleeLoc(): t.Expression {
+          const loc = callee.node.loc
+          return t.objectExpression([
+            t.objectProperty(t.identifier('filename'), state.filename ?
+              t.stringLiteral(state.filename.replace(state.cwd, '')) : t.identifier('undefined')),
+            t.objectProperty(t.identifier('line'), t.numericLiteral(loc?.start?.line || 0)),
+            t.objectProperty(t.identifier('column'), t.numericLiteral(loc?.start?.column || 0)),
+          ])
         }
       },
     },
